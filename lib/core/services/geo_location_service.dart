ccc39ce00bb21274b4432e14d4d36ff1bde3d799
zeyadtarek1999 <104107433+zeyadtarek1999@users.noreply.github.com>
2024-09-15 07:40:58 +0300

    done

--- lib/core/services/geo_location_service.dart
+++ lib/core/services/geo_location_service.dart
@@ -1,28 +1,50 @@
-import 'dart:io';
 import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 class LocationService {
   Future<Position?> getCurrentLocation() async {
-    if (isSimulator()) {
-      // Return a mock location for simulators
-      return Position(
-        latitude: 37.7749,
-        longitude: -122.4194,
-        timestamp: DateTime.now(),
-        accuracy: 1.0,
-        altitude: 1.0,
-        heading: 1.0,
-        speed: 1.0,
-        speedAccuracy: 1.0,
-        altitudeAccuracy: 1.0,
-        headingAccuracy: 1.0,
-        isMocked: true, // Indicate that the location is mocked
-      );
+    bool isPermissionGranted = await _checkLocationPermission();
+
+    if (isPermissionGranted) {
+      if (isSimulator()) {
+        // Use a default location for simulator (e.g., San Francisco)
+        return Position(
+          latitude: 37.7749,
+          longitude: -122.4194,
+          timestamp: DateTime.now(),
+          accuracy: 1.0,
+          altitude: 1.0,
+          heading: 1.0,
+          speed: 1.0,
+          speedAccuracy: 1.0,
+          altitudeAccuracy: 1.0, // Added required parameter
+          headingAccuracy: 1.0, // Added required parameter
+          isMocked: false, // Added required parameter
+        );
+      } else {
+        return await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high,
+        );
+      }
     } else {
-      return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-      );
+      // If permission is denied, return null or handle accordingly
+      return null;
     }
+  }
+
+  Future<bool> _checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    if (permission == LocationPermission.whileInUse || permission == LocationPermission.always) {
+      return true;
+    }
+
+    return false; // Permission was denied or denied forever
   }
 
   bool isSimulator() {
